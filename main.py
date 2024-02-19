f69408ab35bc94c687f67459c8549cd6cb8490fa
Bernardo-07 <bernardoduarte271@gmail.com>
2024-02-19 08:31:55 -0300

    Adjustments

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ data = '4340 34 10x10 - KV8.csv'
 temperature = []
 y = []
 
-with open(data, mode='r') as file:
-    file_csv = csv.reader(file)
+with open(data, mode='r') as file: #abre o arquivo no modo leitura
+    file_csv = csv.reader(file) #cria uma variável para receber a leitura do arquivo
+    next(file_csv) #pula o cabeçalho
 
-    for line in file_csv:
-        temperature.append(line[0])
-        y.append(line[1])
+    for line in file_csv: #itera sobre cada linha da variavel
+        temperature.append(float(line[0]))
+        y.append(float(line[1]))
+        #extrai os dados transformanddo em float
 
 print(temperature)
 print(y)
