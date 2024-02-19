f16038ef1aab9549b7f7a475b904ca3226cd00cc
Bernardo-07 <bernardoduarte271@gmail.com>
2024-02-19 08:27:30 -0300

    extracting data from the .csv file

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import least_squares
 import csv
+
+data = '4340 34 10x10 - KV8.csv'
+temperature = []
+y = []
+
+with open(data, mode='r') as file:
+    file_csv = csv.reader(file)
+
+    for line in file_csv:
+        temperature.append(line[0])
+        y.append(line[1])
+
+print(temperature)
+print(y)
