70947ebea84b56ba44a3fe3d7c7d4e989e2ccf4e
Bernardo-07 <bernardoduarte271@gmail.com>
2024-02-22 15:02:35 -0300

    plot adjustments

--- main.py
+++ main.py
@@ -21,13 +21,6 @@ data = '4340 34 10x10 - KV8.csv'
 temperature = []
 y = []
 
-for i in range(0, len(temperature)):
-    for j in range(0, len(temperature)-i-1):
-        if temperature[j] > temperature[j+1]:
-            # Trocar os elementos se estiverem fora de ordem
-            temperature[j], temperature[j+1] = temperature[j+1], temperature[j]
-            y[j], y[j+1] = y[j+1], y[j]
-
 with open(data, mode='r') as file: #abre o arquivo no modo leitura
     file_csv = csv.reader(file) #cria uma variável para receber a leitura do arquivo
     next(file_csv) #pula o cabeçalho
@@ -36,6 +29,13 @@ with open(data, mode='r') as file: #abre o arquivo no modo leitura
         temperature.append(float(line[0]))
         y.append(float(line[1]))
         #extrai os dados transformanddo em float
+
+for i in range(0, len(temperature)):
+    for j in range(0, len(temperature)-i-1):
+        if temperature[j] > temperature[j+1]:
+            # Trocar os elementos se estiverem fora de ordem
+            temperature[j], temperature[j+1] = temperature[j+1], temperature[j]
+            y[j], y[j+1] = y[j+1], y[j]
 
 #método de mínimos quadrados para achar o melhor valor para os parametros DBTT, C e D
 initial_params = [-85, 35, 0.0256] #chute inicial
@@ -50,5 +50,10 @@ Kv = [None] * len(y)
 for i in range(0, len(y)):
     Kv[i] = function(temperature[i], DBTT, C, D)
 
+plt.title('Gráfico da Tangente Hiperbólica Assimétrica')
+plt.scatter(temperature, y, color="r", marker="D")
 plt.plot(temperature, Kv)
+plt.xlabel('T (°C)')
+plt.ylabel('Kv (J)')
+plt.grid(True)
 plt.show()
